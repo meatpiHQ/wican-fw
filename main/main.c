4fa6e0fa985c44ca91aa356d99935044e598d2e6
meatpi <ali@meatpi.com>
2022-06-10 19:34:20 +1000

    Changed ble name

--- main/main.c
+++ main/main.c
@@ -299,7 +299,7 @@ void app_main(void)
         uint8_t derived_mac_addr[6] = {0};
 
         ESP_ERROR_CHECK(esp_read_mac(derived_mac_addr, ESP_MAC_WIFI_SOFTAP));
-        sprintf((char *)uid,"WiCAN_%02x%02x%02x%02x%02x%02x",
+        sprintf((char *)uid,"WiC_%02x%02x%02x%02x%02x%02x",
                 derived_mac_addr[0], derived_mac_addr[1], derived_mac_addr[2],
                 derived_mac_addr[3], derived_mac_addr[4], derived_mac_addr[5]);
     	ble_init(&xmsg_ble_tx_queue, &xMsg_Rx_Queue, CONNECTED_LED_GPIO_NUM, pass, &uid[0]);
