69dbd57fe0aeda55cfe888fac5619f67110419f5
meatpiHQ <94690098+meatpiHQ@users.noreply.github.com>
2024-12-04 01:51:14 +1100

    Update config_server.c

--- main/config_server.c
+++ main/config_server.c
@@ -1122,18 +1122,18 @@ static esp_err_t upload_post_handler(httpd_req_t *req)
         return ESP_FAIL;
     }
 
-    /* File cannot be larger than a limit */
-    if (req->content_len > MAX_FILE_SIZE)
-    {
-        ESP_LOGE(TAG, "File too large : %d bytes", req->content_len);
-        /* Respond with 400 Bad Request */
-        httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST,
-                            "File size must be less than "
-                            MAX_FILE_SIZE_STR "!");
-        /* Return failure to close underlying connection else the
-         * incoming file content will keep the socket busy */
-        return ESP_FAIL;
-    }
+    // /* File cannot be larger than a limit */
+    // if (req->content_len > MAX_FILE_SIZE)
+    // {
+    //     ESP_LOGE(TAG, "File too large : %d bytes", req->content_len);
+    //     /* Respond with 400 Bad Request */
+    //     httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST,
+    //                         "File size must be less than "
+    //                         MAX_FILE_SIZE_STR "!");
+    //     /* Return failure to close underlying connection else the
+    //      * incoming file content will keep the socket busy */
+    //     return ESP_FAIL;
+    // }
 
     ESP_LOGI(TAG, "Receiving file : %s...", filename);
 
