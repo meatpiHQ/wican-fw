18668a045e39baf1d9c93d5398b33be64aae996b
meatpiHQ <94690098+meatpiHQ@users.noreply.github.com>
2024-12-13 01:23:25 +1100

    clean up

--- main/autopid.h
+++ main/autopid.h
@@ -21,16 +21,9 @@
 #ifndef __AUTO_PID_H__
 #define __AUTO_PID_H__
 
-#define BUFFER_SIZE 512
-#define QUEUE_SIZE 10
+#define BUFFER_SIZE 256
+#define QUEUE_SIZE 20
 
-typedef enum
-{
-    INIT_ELM327 = 0,
-    CONNECT_NOTIFY,
-    DISCONNECT_NOTIFY,
-    READ_PID
-} autopid_state_t;
 
 typedef struct {
     uint8_t data[BUFFER_SIZE];
@@ -39,60 +32,9 @@ typedef struct {
 
 typedef enum
 {
-    MQTT_TOPIC = 0,
-    MQTT_WALLBOX
-} send_to_type_t;
-
-typedef struct {
-    char *name;              // Name
-    char *pid_init;          // PID init string
-    char *pid_command;       // PID command string
-    char *expression;        // Expression string
-    char *destination;       // Example: file name or mqtt topic
-    int64_t timer;              // Timer for managing periodic actions
-    uint32_t period;            // Period in ms frequency of data collection or action
-    uint8_t expression_type;    // Expression type evaluates data from sensors etc
-    uint8_t type;               // Log type, could be MQTT or file-based
-}__attribute__((aligned(1),packed)) pid_req_t ;
-
-typedef enum
-{
     SENSOR = 0,
     BINARY_SENSOR = 1,
 } sensor_type_t;
-
-typedef struct
-{
-    char *name;
-    char *expression;
-    char *unit;
-    char *class;
-    sensor_type_t sensor_type;
-} parameter_data_t;
-
-typedef struct
-{
-    char *pid;
-    parameter_data_t *parameters;  
-    int parameter_count;
-    char *pid_init;
-} pid_data_t;
-
-typedef struct
-{
-    char *car_model;
-    char *init;
-    pid_data_t *pids;              
-    int pid_count;
-    int64_t cycle_timer;  
-    char* selected_car_model;
-    char* destination;
-    char* grouping;
-    uint32_t cycle;
-    uint8_t car_specific_en;
-    uint8_t ha_discovery_en;
-} car_model_data_t;
-
 
 ////////////////
 
@@ -149,6 +91,8 @@ typedef struct
     char* specific_init;
     char* selected_car_model;
     char* grouping;
+    destination_type_t group_destination_type;
+    char* group_destination;    //"destination"
     bool pid_std_en;
     bool pid_custom_en;
     bool pid_specific_en;
@@ -161,14 +105,14 @@ typedef struct
 
 ////////////////
 
-typedef struct 
-{
-    char *data;              // Pointer to a dynamically allocated string
-    SemaphoreHandle_t mutex; // Mutex to protect access to the data
-} autopid_data_t;
+// typedef struct 
+// {
+//     char *data;              // Pointer to a dynamically allocated string
+//     SemaphoreHandle_t mutex; // Mutex to protect access to the data
+// } autopid_data_t;
 
 void autopid_parser(char* str, uint32_t len, QueueHandle_t *q);
-void autopid_init(char* id, char *config_str);
+void autopid_init(char* id);
 char *autopid_data_read(void);
 bool autopid_get_ecu_status(void);
 char* autopid_get_config(void);
