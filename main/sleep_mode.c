ea58a100df33a8de59ea7f9678b0d65ae09fb3c3
meatpiHQ <94690098+meatpiHQ@users.noreply.github.com>
2025-03-10 18:55:39 +1100

    Update sleep_mode.c

--- main/sleep_mode.c
+++ main/sleep_mode.c
@@ -802,10 +802,11 @@ void light_sleep_task(void *pvParameters)
         sleep_voltage = 13.1f;
     }
 
-    if(config_server_get_wakeup_volt(&wakeup_voltage) == -1) 
-	{
-        wakeup_voltage = 13.4f;
-    }
+    // if(config_server_get_wakeup_volt(&wakeup_voltage) == -1) 
+	// {
+    //     wakeup_voltage = 13.4f;
+    // }
+    wakeup_voltage = sleep_voltage + 0.1f;
     
 	if(config_server_get_sleep_time(&sleep_time) == -1)
 	{
@@ -887,7 +888,7 @@ void light_sleep_task(void *pvParameters)
 					{
                         ESP_LOGI(TAG, "Voltage above wakeup threshold, starting wakeup timer");
                         current_state = STATE_WAKE_PENDING;
-                        wc_timer_set(&wakeup_timer, 2000); // 2 second timer for stable voltage
+                        wc_timer_set(&wakeup_timer, 1000); // 2 second timer for stable voltage
                     }
                     break;
 
@@ -920,17 +921,26 @@ void light_sleep_task(void *pvParameters)
         }
 
         // Handle sleep entry
-        if(current_state == STATE_SLEEPING && gpio_get_level(OBD_READY_PIN) == 1) 
+        // if(current_state == STATE_SLEEPING && gpio_get_level(OBD_READY_PIN) == 1) 
+        // {
+        //     // adc_continuous_stop(handle);
+        //     ESP_LOGW(TAG, "Sleep...");
+        //     esp_sleep_enable_timer_wakeup(2*1000000);
+        //     esp_light_sleep_start();
+        //     ESP_LOGW(TAG, "Wakeup...");
+        //     if(gpio_get_level(OBD_READY_PIN) == 0)
+        //     {
+        //         esp_restart();
+        //     }
+        //     // adc_continuous_start(handle);
+        // }
+        if(current_state == STATE_SLEEPING) 
         {
             // adc_continuous_stop(handle);
             ESP_LOGW(TAG, "Sleep...");
             esp_sleep_enable_timer_wakeup(2*1000000);
             esp_light_sleep_start();
             ESP_LOGW(TAG, "Wakeup...");
-            if(gpio_get_level(OBD_READY_PIN) == 0)
-            {
-                esp_restart();
-            }
             // adc_continuous_start(handle);
         }
         if(current_state != STATE_SLEEPING)
