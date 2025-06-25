99018f9f556d7bb6c9ef07662231d8466f411639
askpatrickw <4002194+askpatrickw@users.noreply.github.com>
2025-06-24 23:42:55 -0700

    Correct markdown format for Heading 1

--- .vehicle_profiles/merge.js
+++ .vehicle_profiles/merge.js
@@ -111,6 +111,7 @@ THIS FILE WAS GENERATED! DO NOT UPDATE OR YOUR CHANGES ARE LOST!
 
 -->
 # Supported Parameters
+
 This table lists all supported parameters, their descriptions, and settings as used in WiCAN vehicle profiles.
 
 | Parameter | Description | Settings |
