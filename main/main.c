463b7a8fb7517ce793a41f33a39a35cc2157717d
meatpiHQ <94690098+meatpiHQ@users.noreply.github.com>
2025-12-31 15:12:45 +1100

    Add USB Ethernet and ACM CLI test integration
    Integrated USB Ethernet host and ACM CLI modules under ESPNETLINK_TEST. Added configuration and initialization logic for USB Ethernet and ACM CLI, including network settings and device path, to support testing and development.

--- main/main.c
+++ main/main.c
@@ -79,6 +79,11 @@
 #include "config_mode.h"
 #include "driver/rtc_io.h"
 
+#include "usb_eth_host.h"
+#include "usb_acm_cli.h"
+
+#include "lwip/ip4_addr.h"
+
 #define TAG 		__func__
 #define USB_ID_PIN					39
 #define USB_OTG_PWR_EN				10
@@ -1151,6 +1156,15 @@ void app_main(void)
 	// esp_log_level_set("WiFi_Manager", ESP_LOG_INFO);
 	// esp_log_level_set("HA_WEBHOOK_CFG", ESP_LOG_INFO);
 	// esp_log_level_set("HA_WEBHOOK_HTTP", ESP_LOG_INFO);
+	#if defined(ESPNETLINK_TEST)
+	esp_log_level_set("usb_eth_cdc_ecm", ESP_LOG_INFO);
+	esp_log_level_set("usb_eth_asix", ESP_LOG_INFO);
+	esp_log_level_set("usb_eth_rtl8152", ESP_LOG_INFO);
+	esp_log_level_set("usb_eth_netif", ESP_LOG_INFO);
+	esp_log_level_set("usb_eth_host", ESP_LOG_INFO);
+	esp_log_level_set("usb_eth_cdc_ncm", ESP_LOG_INFO);
+	esp_log_level_set("usb_acm_cli", ESP_LOG_INFO);
+	#endif
 
 	#if HARDWARE_VER == WICAN_V300 || HARDWARE_VER == WICAN_USB_V100
     gpio_set_level(PWR_LED_GPIO_NUM, 1);
@@ -1168,12 +1182,57 @@ void app_main(void)
 	// led_set_level(100, 0, 200);  // Set blue color
 	// led_set_pattern_ms(LED_BLUE, &breathing_pattern);
 	// led_set_pattern_ms(LED_GREEN, &breathing_pattern);
-	if(gpio_get_level(USB_ID_PIN) == 0)
+	#if defined(ESPNETLINK_TEST)
+	vTaskDelay(pdMS_TO_TICKS(6000));
+	bool start_usb_acm_cli = false;
+	if (gpio_get_level(USB_ID_PIN) == 0)
 	{
-		usb_host_init();
+		printf("USB ID pin is low\n");
+		gpio_reset_pin(GPIO_NUM_11);
+		gpio_set_direction(GPIO_NUM_11, GPIO_MODE_OUTPUT);
+		gpio_set_level(GPIO_NUM_11, 1);
+		vTaskDelay(pdMS_TO_TICKS(2000));
+		usb_eth_host_config_t usb_eth_cfg;
+		memset(&usb_eth_cfg, 0, sizeof(usb_eth_cfg));
+		usb_eth_cfg.enable = true;
+		usb_eth_cfg.allowed_driver_mask = USB_ETH_HOST_DRIVER_MASK_ALL;
+		usb_eth_cfg.bus_id = 0;
+		usb_eth_cfg.reg_base = 0;
+		usb_eth_cfg.gpio.vbus_en_gpio = -1;
+		usb_eth_cfg.gpio.vbus_en_active_high = false;
+		usb_eth_cfg.gpio.vbus_on_delay_ms = 0;
+
+		// Network config (later: drive these from UI/config server)
+		usb_eth_cfg.netif.mode = USB_ETH_HOST_IP_MODE_DHCP;
+		usb_eth_cfg.netif.prefer_as_default_route = true;
+		IP4_ADDR((ip4_addr_t *)&usb_eth_cfg.netif.static_ip.ip, 192, 168, 20, 200);
+		IP4_ADDR((ip4_addr_t *)&usb_eth_cfg.netif.static_ip.netmask, 255, 255, 255, 0);
+		IP4_ADDR((ip4_addr_t *)&usb_eth_cfg.netif.static_ip.gw, 192, 168, 20, 1);
+
+		usb_eth_host_start(&usb_eth_cfg);
+		start_usb_acm_cli = true;
 	}
+	#endif
     #endif
 
 	cmdline_init();
+
+	#if defined(ESPNETLINK_TEST)
+	if (start_usb_acm_cli)
+	{
+		usb_acm_cli_config_t acm_cfg;
+		memset(&acm_cfg, 0, sizeof(acm_cfg));
+		acm_cfg.enable = true;
+		acm_cfg.dev_path = "/dev/ttyACM0";
+		acm_cfg.reconnect_delay_ms = 1000;
+		acm_cfg.assert_dtr = true;
+		acm_cfg.assert_rts = false;
+		acm_cfg.line_state_delay_ms = 200;
+		acm_cfg.rx_task_stack_size = 4096;
+		acm_cfg.rx_task_priority = 5;
+		acm_cfg.rx_task_core_id = -1;
+		(void)usb_acm_cli_start(&acm_cfg);
+	}
+	#endif
 }
 
